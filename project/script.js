9d886464eae5cb89265d0ae6e0e54b62157eb334
Alvin Agana <alvagana@ucdavis.edu>
2021-11-11 23:55:27 -0800

    added intro content, finished navbar, and basic script

---
+++ project/script.js
@@ -0,0 +1,25 @@
+(function() {
+
+    'use strict';
+    console.log("reading js");
+
+    const navItems = document.querySelectorAll('nav ul:first-child li');
+    const navLinks = document.querySelectorAll('nav ul:first-child li a');
+    
+    navItems.forEach(function(item, i) {
+        item.addEventListener("click", function() {
+            event.preventDefault();
+
+            let targetID = navLinks[i].getAttribute('href');
+            let targetAnchor = document.getElementById(targetID);
+            let originalTop = Math.floor(targetAnchor.getBoundingClientRect().top) - 70;
+            window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+        });
+    })
+
+    const githubLink = document.querySelector('nav ul:last-child li');
+    githubLink.addEventListener("click", function() {
+        window.open("https://github.com/Diean233/171Group11");
+    })
+})();
+
