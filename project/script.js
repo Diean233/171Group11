686fd10cc6f03edbdeb63e0436e4ef585fefb932
leonzh2k <leonzh2k@gmail.com>
2021-11-13 21:41:16 -0800

    implemented back to top button

--- project/script.js
+++ project/script.js
@@ -9,13 +9,19 @@
     navItems.forEach(function(item, i) {
         item.addEventListener("click", function(e) {
             e.preventDefault();
-
             let targetID = navLinks[i].getAttribute('href');
             let targetAnchor = document.getElementById(targetID);
             let originalTop = Math.floor(targetAnchor.getBoundingClientRect().top) - 70;
             window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
         });
     });
+
+    const backToTopButton = document.querySelector('#group-info');
+    backToTopButton.addEventListener("click", function() {
+        let targetAnchor = document.getElementById("#intro");
+        let originalTop = Math.floor(targetAnchor.getBoundingClientRect().top) - 70;
+        window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+    })
 
     const githubLink = document.querySelector('nav ul:last-child li');
     githubLink.addEventListener("click", function() {
